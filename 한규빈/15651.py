04f51665435b392f1834285a026e8019f3bb9cba
Gyubin Han <89185979+Gyubin-Han@users.noreply.github.com>
2023-08-27 17:16:36 +0900

    add: Gyubin-Han 15651.py

---
+++ 한규빈/15651.py
@@ -0,0 +1,14 @@
+n,m=map(int,input().split())
+arr=[]
+
+def bt():
+    if len(arr)==m:
+        print(*arr)
+        return
+    
+    for i in range(1,n+1):
+        arr.append(i)
+        bt()
+        arr.pop()
+
+bt()
